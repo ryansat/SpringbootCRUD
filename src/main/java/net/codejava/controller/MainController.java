3477c7a59c3a571186c2924cf5cc9bc33ca577f0
Fajar Satria <fajarsat@hotmail.com>
2020-05-03 13:12:17 +0700

    add user role credential
    add user role credential checking

--- src/main/java/net/codejava/controller/MainController.java
+++ src/main/java/net/codejava/controller/MainController.java
@@ -1,6 +1,9 @@
 package net.codejava.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +17,20 @@ public class MainController {
 	
 	@RequestMapping("/")
     public String root() {
-        return "list_product";
+        try{
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            boolean hasUserRole = authentication.getAuthorities().stream()
+                      .anyMatch(r -> r.getAuthority().equals("ADMIN"));
+            if (hasUserRole == true){
+                return "list_product";
+            }else{
+                return "list_identity";
+            }
+                   
+        }
+        catch(Exception e){
+        }
+        return "list_identity";
     }
 
     @GetMapping("/login")
@@ -23,7 +39,7 @@ public class MainController {
     }
 
     @GetMapping("/user")
-    public String userIndex() {
-        return "list_product";
+    public String userIndex(Model model) {
+        return "list_identity";
     }
 }
